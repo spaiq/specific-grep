2d6933d4a0ad46077cc008c8905182ce34ff2786
spaiq <jb50996@zut.edu.pl>
2023-04-02 18:53:03 +0200

    feat: add searchDirectoryForString() function

--- specific_grep.cpp
+++ specific_grep.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <filesystem>
 #include <thread>
+#include <future>
 
 namespace fs = std::filesystem;
 
@@ -48,6 +49,50 @@ std::vector<std::tuple<std::thread::id, std::string, int, std::string>> searchFi
 
 	// Return the vector of search results
 	return results;
+}
+
+
+/**
+ * Search a directory and its subdirectories for files containing a given string.
+ *
+ * @param search_string The string to search for.
+ * @param directory_path The path to the directory to search.
+ * @param thread_count The number of threads to use for the search.
+ * @return A pair containing a vector of tuples representing the search results and an integer representing the total number of files searched.
+ */
+std::pair<std::vector<std::tuple<std::thread::id, std::string, int, std::string>>, int> searchDirectoryForString(const std::string& search_string, const std::string& directory_path, const int thread_count) {
+	// Create a vector of paths to all regular files in the directory and its subdirectories.
+	std::vector<fs::path> files_to_search;
+	for (const auto& file : fs::recursive_directory_iterator(directory_path)) {
+		if (fs::is_regular_file(file)) {
+			files_to_search.push_back(file.path());
+		}
+	}
+
+	// Calculate the number of files to search per thread.
+	const int files_per_thread = files_to_search.size() / thread_count;
+
+	// Create a vector of futures representing the search results for each thread.
+	std::vector<std::future<std::vector<std::tuple<std::thread::id, std::string, int, std::string>>>> futures;
+	for (int i = 0; i < thread_count; ++i) {
+		const int start_index = i * files_per_thread;
+		const int end_index = (i + 1) == thread_count ? files_to_search.size() : (i + 1) * files_per_thread;
+		const std::vector<fs::path> files_subset(files_to_search.begin() + start_index, files_to_search.begin() + end_index);
+
+		// Launch a new thread to search the subset of files.
+		std::future<std::vector<std::tuple<std::thread::id, std::string, int, std::string>>> result = std::async(std::launch::async, searchFilesForString, std::cref(search_string), files_subset);
+		futures.emplace_back(std::move(result));
+	}
+
+	// Collect the results from each thread and combine them into a single vector.
+	std::vector<std::tuple<std::thread::id, std::string, int, std::string>> results;
+	for (auto& future : futures) {
+		std::vector<std::tuple<std::thread::id, std::string, int, std::string>> result = future.get();
+		results.insert(results.end(), result.begin(), result.end());
+	}
+
+	// Return a pair containing the search results and the total number of files searched.
+	return std::make_pair(results, files_to_search.size());
 }
 
 
